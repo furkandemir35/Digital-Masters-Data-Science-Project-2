a509d6902007df9aa886230813b40fa49a417712
furkandemir35 <63724277+furkandemir35@users.noreply.github.com>
2020-05-19 02:02:02 +0300

    Add files via upload

---
+++ project-2/app/run.py
@@ -0,0 +1,160 @@
+import json
+import plotly
+import pandas as pd
+import numpy as np
+import pdb
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import plotly
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar,Pie,Scatter,Histogram
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+from wordcloud import WordCloud, STOPWORDS 
+import random
+from collections import Counter
+
+import re
+import nltk
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import stopwords 
+from nltk import word_tokenize
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+stemmer = PorterStemmer()
+lemmatiser = WordNetLemmatizer()
+cachedStopWords = stopwords.words("english")
+
+app = Flask(__name__)
+
+def tokenize(text):
+   
+    posts = text
+    token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'link', posts)
+    token = re.sub("[^a-zA-Z]", " ", token)
+    token = re.sub(' +', ' ', token).lower()
+    token = " ".join([lemmatiser.lemmatize(w) for w in token.split(' ') if w not in cachedStopWords])
+    token=nltk.word_tokenize(token)
+    return token
+# load data
+engine = create_engine('sqlite:///data/DisasterResponse.db')
+df = pd.read_sql_table('TableName', engine)
+#pdb.set_trace()
+# load model
+model = joblib.load(r"models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    #pdb.set_trace()
+    genre_names = list(genre_counts.index)
+    
+    complete_list = ' '.join([i for i in df['message']])
+    counts = Counter(tokenize(complete_list))
+    words= [word for word, word_count in Counter(counts).most_common(30)]
+    colors = [plotly.colors.DEFAULT_PLOTLY_COLORS[random.randrange(1, 10)] for i in range(30)]
+    weights = list(range(30))[::-1]
+    
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+                {
+            'data': [
+                Pie(
+                    labels=genre_names,
+                    values=genre_counts,
+                    pull=.2,
+                    hole=.2,
+                    #colorway='blues',
+                    textposition='outside',
+                    textinfo='percent'
+                )
+            ],
+
+            'layout': {
+                'title': 'Pie Chart of Message Genres'
+            }
+        },
+        {
+            'data': [
+                Scatter(
+                    x=[random.random() for i in range(30)],
+                    y=list(range(30)),
+                    mode='text',
+                    text=words,
+                    marker={'opacity': 0.3},
+                    textfont={'color': colors}
+                )
+            ],
+
+            'layout': {
+                'title': 'WordCloud - Top Words',
+                'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
+                'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
+                'hoverinfo':'none'
+            }
+        }
+    ]
+    
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    
+    classification_labels = model.predict([query])[0]
+    
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ project-2/models/train_classifier.py
@@ -0,0 +1,180 @@
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from sklearn.model_selection import GridSearchCV
+import nltk
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import stopwords 
+from nltk import word_tokenize
+import re
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import *
+import seaborn as sns
+import tqdm
+import pdb
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import roc_auc_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import confusion_matrix
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+pd.set_option('display.max_colwidth', 0)
+
+stemmer = PorterStemmer()
+lemmatiser = WordNetLemmatizer()
+cachedStopWords = stopwords.words("english")
+
+def load_data(database_filepath):
+    
+    #Returns: Dataframe splitted into message and their tags
+    
+    engine = create_engine('sqlite:///'+database_filepath)
+    name_of_the_table='TableName'
+    df = pd.read_sql_table(name_of_the_table,engine)
+    X = df[['message']]
+    Y = df.drop(['id','message','original','genre'],axis=1)
+    categories = list(Y.columns.values)
+    return X,Y,categories,df
+
+def tokenize(text):
+
+    #Returns: Tokenized Text,post usage of lemmatiser
+    
+    posts = text
+    tokens = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'link', posts)
+    tokens = re.sub("[^a-zA-Z]", " ", tokens)
+    tokens = re.sub(' +', ' ', tokens).lower()
+    tokens = " ".join([lemmatiser.lemmatize(w) for w in tokens.split(' ') if w not in cachedStopWords])
+    tokens=nltk.word_tokenize(tokens)
+    return tokens
+
+
+def build_model():
+    '''
+    Returns: pipeline with customer tokenizer and multi output classifier
+    '''
+    pipeline = Pipeline([
+                ('tfidf', TfidfVectorizer(tokenizer=tokenize)),
+                ('clf', MultiOutputClassifier(
+                                              RandomForestClassifier
+                                              (n_estimators=100, random_state=1), n_jobs=-1)),
+            ])
+    return pipeline
+
+
+def evaluate_model(model, X_test, X_train, category_names,train,test):
+
+    #Returns: Tuned Model
+       
+    categories=category_names
+    # compute the testing accuracy
+    prediction = model.predict(X_test)
+    predictionTrain = model.predict(X_train)
+    #printing accuracy score for each categories  - For rubric "The accuracy, precision and recall for the test set is outputted for each category."
+    i=0
+    for category in categories:
+        print('Test accuracy for category - {} is {}'.format(category,accuracy_score(test[category].values, prediction[:,i])))
+        print("Classification Report")
+        print(classification_report(test[category], prediction[:,i]))
+        print("Confusion Matrix")
+        print(confusion_matrix(test[category], prediction[:,1]))
+        print("***"*30)
+        i=i+1
+    print('Train accuracy is {}'.format(accuracy_score(train[categories].values.flatten(), predictionTrain.flatten())))
+    print('Test accuracy is {}'.format(accuracy_score(test[categories].values.flatten(), prediction.flatten())))
+    print("Classification Report for Train")
+    print(classification_report(train[categories].values.flatten(), predictionTrain.flatten()))
+    print("Classification Report for Test")
+    print(classification_report(test[categories].values.flatten(), prediction.flatten()))
+    print("Train Confusion Matrix")
+    print(confusion_matrix(train[categories].values.flatten(), predictionTrain.flatten()))
+    print("Test Confusion Matrix")
+    print(confusion_matrix(test[categories].values.flatten(), prediction.flatten()))
+    parameters = {
+        'clf__estimator__criterion': ['gini', 'entropy'],
+        'clf__estimator__n_estimators': [50, 100],
+    }
+    grid_search_tune = GridSearchCV(model, parameters, cv=2, n_jobs=-1, verbose=3)
+    #pdb.set_trace()                                
+    grid_search_tune.fit(X_train, train[categories])
+    print("Best parameters set ")
+    print(grid_search_tune.best_estimator_.steps)
+    best_parameters = grid_search_tune.best_estimator_.get_params()
+    for param_name in sorted(parameters.keys()):
+        print ('\t%s: %r' % (param_name, best_parameters[param_name]))
+    print("Checking Prediction for Tuned Model")    
+    prediction = grid_search_tune.predict(X_test)
+    print('Train accuracy is {}'.format(accuracy_score(train[categories].values.flatten(), predictionTrain.flatten())))    
+    print("Classification Report for Test")
+    print(classification_report(test[categories].values.flatten(), prediction.flatten()))
+    print("Test Confusion Matrix")
+    print(confusion_matrix(test[categories].values.flatten(), prediction.flatten()))
+    #printing accuracy score for each categories  - For rubric "The accuracy, precision and recall for the test set is outputted for each category."
+    i=0
+    for category in categories:
+        print('Test accuracy for category - {} is {}'.format(category,accuracy_score(test[category].values, prediction[:,i])))
+        print("Classification Report")
+        print(classification_report(test[category], prediction[:,i]))
+        print("Confusion Matrix")
+        print(confusion_matrix(test[category], prediction[:,1]))
+        print("***"*30)
+        i=i+1
+
+    return grid_search_tune
+
+def save_model(model, model_filepath):
+    '''
+    Saving the model to pickle
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names,df = load_data(database_filepath)
+        #X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        train, test = train_test_split(df, random_state=42, test_size=0.2, shuffle=True)
+        X_train = train.message
+        X_test = test.message
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, train[category_names])
+
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, X_train, category_names,train,test)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
