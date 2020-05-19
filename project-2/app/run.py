6016e92b3f1a2c9c70c6adf6fb4aaa2e85d57f1b
furkandemir35 <63724277+furkandemir35@users.noreply.github.com>
2020-05-19 14:34:13 +0300

    Update run.py

--- project-2/app/run.py
+++ project-2/app/run.py
@@ -31,7 +31,10 @@ cachedStopWords = stopwords.words("english")
 app = Flask(__name__)
 
 def tokenize(text):
-   
+    '''
+    Input: Text
+    Returns: Cleaned and tokenized text to model
+    '''
     posts = text
     token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'link', posts)
     token = re.sub("[^a-zA-Z]", " ", token)
